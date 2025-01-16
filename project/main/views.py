ae7bcb819af5289a0b0849836565c1e6be2cfed3
Slayik <hzq.hihah@gmail.com>
2025-01-16 21:49:01 +0300

    3

--- project/main/views.py
+++ project/main/views.py
@@ -2,8 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+#Главная страница
 def index(request):
-    return render(request,)
+    context: dict[str, str] = {
+        'title': 'Home',
+        'content': 'Главная страница магазина'
+    }
+    return render(request, 'main/index.html', context)
 
+#Страница О нас
 def about(request):
     return HttpResponse('About page')
+
