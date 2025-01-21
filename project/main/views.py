037d7444bcc6bea23692adf6d2a58d874d961e5f
Slayik <hzq.hihah@gmail.com>
2025-01-21 23:01:29 +0300

    crush

--- project/main/views.py
+++ project/main/views.py
@@ -1,33 +1,33 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-#Главная страница
+# Главная страница
 def index(request):
-    context: dict[str, str] = {
+    context = {
         'title': 'Home',
         'content': 'Главная страница магазина'
     }
     return render(request, 'main/index.html', context)
 
-#Страница О нас
+# Страница О нас
 def about(request):
     return render(request, 'main/about.html')
 
+# Страница с отзывами
 def comments(request):
     return render(request, 'main/comments.html')
 
-def index(request):
-    return render(request, 'main/index.html')
-
+# Страница "Мотоциклы в наличии"
 def in_stock(request):
     return render(request, 'main/in_stock.html')
 
+# Страница "Мотоциклы под заказ"
 def to_order(request):
     return render(request, 'main/to_order.html')
 
+# Страница с экипировкой
 def equip(request):
     return render(request, 'main/equip.html')
 
+# Страница с контактами
 def contacts(request):
     return render(request, 'main/contacts.html')
--- project/project/urls.py
+++ project/project/urls.py
@@ -20,11 +20,11 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('', views.index, name = 'index'),
-    path('about/', views.about, name = 'about'),        # URL для страницы О нас
-    path('comments/', views.comments, name='comments'),  # URL для страницы отзывов
-    path('in_stock/', views.index, name='in_stock'),  # URL для страницы В наличии
-    path('to_order/', views.index, name='to_order'),  # URL для страницы Под заказ
-    path('equip/', views.index, name='equip'),  # URL для страницы Экипировка
-    path('contacts/', views.index, name='contacts'),  # URL для страницы Контакты
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),  # Страница "О нас"
+    path('comments/', views.comments, name='comments'),  # Страница "Отзывы"
+    path('in_stock/', views.in_stock, name='in_stock'),  # Страница "В наличии"
+    path('to_order/', views.to_order, name='to_order'),  # Страница "Под заказ"
+    path('equip/', views.equip, name='equip'),  # Страница "Экипировка"
+    path('contacts/', views.contacts, name='contacts'),  # Страница "Контакты"
 ]
