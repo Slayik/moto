4e23a34c3bf4bacb973fbe6eeddbed3204f721cb
Slayik <hzq.hihah@gmail.com>
2025-01-25 17:30:15 +0300

    some cor

--- project/callback/form.py
+++ project/callback/form.py
@@ -1,8 +1,16 @@
 <form id="callbackForm" method="POST">
     {% csrf_token %}
-    <input type="text" id="name" name="name" required>
-    <input type="text" id="phone" name="phone_number" required>
-    <input type="email" id="email" name="email">
-    <textarea id="message" name="message"></textarea>
+    <label for="name">Ваше имя</label>
+    <input type="text" id="name" name="name" placeholder="Введите ваше имя" required>
+    
+    <label for="phone">Ваш номер телефона</label>
+    <input type="text" id="phone" name="phone_number" placeholder="Введите ваш номер телефона" required>
+    
+    <label for="email">Ваш email</label>
+    <input type="email" id="email" name="email" placeholder="example@mail.com">
+    
+    <label for="message">Ваше сообщение</label>
+    <textarea id="message" name="message" placeholder="Введите ваше сообщение"></textarea>
+    
     <button type="submit">Отправить</button>
 </form>
--- project/callback/models.py
+++ project/callback/models.py
@@ -1,12 +1,11 @@
-
 from django.db import models
 
 class CallbackRequest(models.Model):
-    name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=20)
-    email = models.EmailField()  # поле для email
-    message = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
+    email = models.EmailField(verbose_name="Электронная почта")
+    message = models.TextField(blank=True, null=True, verbose_name="Сообщение")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
     def __str__(self):
         return f"{self.name} - {self.phone_number} - {self.email}"
