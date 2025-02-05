23cb979cd1dfd3eb32cd87008691d2d54d10c243
slayik <hzq.hihah@gmail.com>
2025-02-05 20:35:42 +0300

    comments_add

--- project/products/urls.py
+++ project/products/urls.py
@@ -2,8 +2,17 @@
 from django.urls import path
 from . import views
 
+# Определение маршрутов для приложения products
 urlpatterns = [
+    # Маршрут для страницы мотоциклов в наличии
+    # Связан с представлением moto_in_stock_view и доступен по URL '/in_stock/'
     path('in_stock/', views.moto_in_stock_view, name='in_stock'),
+
+    # Маршрут для страницы мотоциклов, доступных под заказ
+    # Связан с представлением moto_to_order_view и доступен по URL '/to_order/'
     path('to_order/', views.moto_to_order_view, name='to_order'),
+
+    # Маршрут для страницы списка экипировки
+    # Связан с представлением equipments_view и доступен по URL '/equip/'
     path('equip/', views.equipments_view, name='equip'),
 ]
--- project/products/views.py
+++ project/products/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .models import MotoInStock, MotoToOrder, Equipments
 
+# Представление для отображения мотоциклов в наличии
+# Получает все объекты модели MotoInStock и передаёт их в шаблон 'in_stock.html'
 def moto_in_stock_view(request):
-    moto_in_stock = MotoInStock.objects.all()
-    return render(request, 'main/in_stock.html', {'moto_in_stock': moto_in_stock})
+    moto_in_stock = MotoInStock.objects.all()  # Получение всех мотоциклов в наличии
+    return render(request, 'main/in_stock.html', {'moto_in_stock': moto_in_stock})  # Рендеринг шаблона с контекстом
 
+# Представление для отображения мотоциклов, доступных под заказ
+# Получает все объекты модели MotoToOrder и передаёт их в шаблон 'to_order.html'
 def moto_to_order_view(request):
-    moto_to_order = MotoToOrder.objects.all()
-    return render(request, 'main/to_order.html', {'moto_to_order': moto_to_order})
+    moto_to_order = MotoToOrder.objects.all()  # Получение всех мотоциклов под заказ
+    return render(request, 'main/to_order.html', {'moto_to_order': moto_to_order})  # Рендеринг шаблона с контекстом
 
+# Представление для отображения списка экипировки
+# Получает все объекты модели Equipments и передаёт их в шаблон 'equip.html'
 def equipments_view(request):
-    equipments = Equipments.objects.all()
-    return render(request, 'main/equip.html', {'equipments': equipments})
+    equipments = Equipments.objects.all()  # Получение всей доступной экипировки
+    return render(request, 'main/equip.html', {'equipments': equipments})  # Рендеринг шаблона с контекстом
